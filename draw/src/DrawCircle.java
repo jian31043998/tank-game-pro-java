dec5e4cb12eff9bd4eb282d8fe9a335823966574
Charles Zeng <328305160@qq.com>
2022-06-09 22:50:58 +0800

    draw a tank not convenience

--- draw/src/DrawCircle.java
+++ draw/src/DrawCircle.java
@@ -7,24 +7,33 @@ import java.awt.*;
  */
 public class DrawCircle extends JFrame {
 
-    private MyPanel mp = null;
     public static void main(String[] args) {
         new DrawCircle();
     }
 
+    //Create the frame constructor
     public DrawCircle() {
-        mp = new MyPanel();
+        //Initial the panel
+        MyPanel mp = new MyPanel();
+        //add the panel in the frame
         this.add(mp);
         this.setSize(800, 600);
+        //close this the click x
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
+
     }
 }
 
 class MyPanel extends JPanel {
+    //First open the panel or min or max the screen or repaint or change size
     @Override
     public void paint(Graphics g) {
+        // initial the father class
         super.paint(g);
-        g.drawOval(0, 0, 100, 100);
+        // draw a circle
+        g.setColor(Color.GREEN);
+
+
     }
 }
